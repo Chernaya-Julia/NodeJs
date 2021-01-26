c29bc2ba94905223be948e557c559ba45dec7b58
Julia Chernaya <73664260+Chernaya-Julia@users.noreply.github.com>
2021-01-26 14:31:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const MilitaryResource = require('./script_1.js');
+const Squad = require('./script_2.js');
+let tanks = new MilitaryResource('tanks', 1000, 400);
+let mashineGunner = new MilitaryResource('Mashine Gunner', 1000, 200);
+let squad = new Squad([tanks, mashineGunner]);
